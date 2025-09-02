ccdd8391a9971a0327d5968d830f93ccc6bdf6ca
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-02 16:37:53 +0530

    Hero Page Completed

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,4 +1,52 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const Hero = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create(".hero-title", {
+            type: "chars",
+        })
+
+        const tl = gsap.timeline({
+            delay:1,
+        })
+
+        tl.to(".hero-content", {
+          opacity: 1,
+          y: 0,
+          ease: "power1.inOut",
+        })
+            .to(".hero-scroll", {
+          duration: 1,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out"
+        }, "-=0.5")
+            .from(titleSplit.chars, {
+            yPercent: 200,
+            stagger: 0.02,
+            ease: "power2.out"
+            }, "-=0.75")
+        
+        const heroTL = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".hero-container",
+                start: "1% top",
+                end: "bottom top",
+                scrub: true,
+            }
+        })
+
+        heroTL.to(".hero-container", {
+            rotate: 7,
+            scale: 0.9,
+            yPercent: 30,
+            ease: "power1.inOut"
+        })
+
+    })
+
   return (
     <div className="bg-[#232224]">
       <div className="hero-container relative bg-[#faeade] w-screen h-dvh overflow-hidden">
@@ -7,7 +55,7 @@ const Hero = () => {
           src="public\images\static-img.png"
           alt=""
         />
-        <div className="hero-content relative z-10 w-full h-full flex flex-col items-center justify-center translate-y-10">
+        <div className="hero-content opacity-0 relative z-10 w-full h-full flex flex-col items-center justify-center translate-y-10">
           <div className="overflow-hidden">
             <h1 className="hero-title text-[#523122] font-bold uppercase leading-[9vw] text-[7.8vw] tracking-tight">
               Freaking Delicious
@@ -20,12 +68,12 @@ const Hero = () => {
             className="hero-scroll rotate-[-3deg] border-[.5vw] border-[#faeade]"
           >
             <div className="bg-[#a26833]">
-              <h1 className="hero-title text-[#fce1cd] font-bold uppercase text-[7.8vw] leading-[9vw] tracking-tight px-5 pb-4">
+              <h1 className="text-[#fce1cd] font-bold uppercase text-[7.8vw] leading-[9vw] tracking-tight px-5 pb-4">
                 Protein + Caffeine
               </h1>
             </div>
           </div>
-          <div className="mb-20">
+          <div className="mb-10 mt-10">
             <h2 className="font-[ProximaNova] font-bold text-[#523122] text-center max-w-md px-4 text-md leading-[115%] mt-3">
               Live life to the fullest with SPYLT: Shatter boredom and embrace
               your inner kid with every deliciously smooth chug.
