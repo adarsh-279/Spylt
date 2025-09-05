5c3c0fd5cc8ffab15d80587e31c5736067c828d9
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-05 13:08:14 +0530

    Product Page Completed

---
+++ src/pages/Products.jsx
@@ -0,0 +1,46 @@
+import ProductSlider from "../components/ProductSlider";
+import ProductTitle from "../components/ProductTitle";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+const Products = () => {
+  const innerRef = useRef(null);
+
+  useGSAP(() => {
+    const scrollAmt = innerRef.current.scrollWidth - window.innerWidth;
+
+    gsap.to(innerRef.current, {
+      x: `-${scrollAmt}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".product-section",
+        start: "-10% top",
+        end: `+=${scrollAmt}`,
+        scrub: true,
+        pin:true,
+      },
+    });
+  });
+
+  return (
+    <div
+      className="product-section relative w-full bg-[#faeade]"
+      style={{ height: "100vh" }}
+    >
+      <div
+        ref={innerRef}
+        className="products-inner relative top-0 left-0 h-screen w-max flex flex-row items-center gap-10"
+      >
+        <div className="product-text w-[60vw] h-full absolute flex items-center justify-center flex-none">
+          <ProductTitle />
+        </div>
+        <div className="product-slider absolute top-0 left-250 h-full">
+          <ProductSlider />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
